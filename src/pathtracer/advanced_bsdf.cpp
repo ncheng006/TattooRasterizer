e669aa9024575d8060da09a58641d3db596d4031
Jojo Chen <jojochen@berkeley.edu>
2023-03-24 12:39:38 -0700

    task 4

--- src/pathtracer/advanced_bsdf.cpp
+++ src/pathtracer/advanced_bsdf.cpp
@@ -44,14 +44,12 @@ namespace CGL {
         // TODO Project 3-2: Part 2
         // Compute Beckmann normal distribution function (NDF) here.
         // You will need the roughness alpha.
-      
-        double a = 0.05; // roughness alpha
 
         double theta = getTheta(h);
-        double exponent = (-tan(theta) * tan(theta)) / (a * a);
+        double exponent = (-tan(theta) * tan(theta)) / (alpha * alpha);
         double numerator = exp(exponent);
-        double denominator = PI * a * a * pow(cos(theta), 4);
-    
+        double denominator = PI * alpha * alpha * pow(cos(theta), 4);
+
 
         return numerator / denominator;
     }
@@ -62,12 +60,13 @@ namespace CGL {
         // You will need both eta and etaK, both of which are Vector3D.
         double costheta = cos(getTheta(wi));
         Vector3D eta_2_k_2 = eta * eta + k * k;
-        
+
         Vector3D rs = (eta_2_k_2 - 2 * eta * costheta + pow(costheta, 2)) / (eta_2_k_2 + 2 * eta * costheta +  pow(costheta, 2));
-        
+
         Vector3D rp = (eta_2_k_2 *  pow(costheta, 2) - 2 * eta * costheta + 1) / (eta_2_k_2 *  pow(costheta, 2) + 2 * eta * costheta + 1);
 
         return (rs + rp)/2;
+    
     }
 
     Vector3D MicrofacetBSDF::f(const Vector3D wo, const Vector3D wi) {
@@ -77,6 +76,10 @@ namespace CGL {
         Vector3D n(0,0,1); // macro surface normal
         Vector3D h = wi + wo; // the half vector
         h.normalize();
+        
+        if (dot(n, wi) < 0 || dot(n, wo) < 0) {
+          return Vector3D();
+        }
 
         Vector3D numerator = F(wi) * G(wo,wi) * D(h);
         Vector3D denominator = 4 * dot(n, wo) * dot(n, wi);
@@ -90,9 +93,34 @@ namespace CGL {
         // *Importance* sample Beckmann normal distribution function (NDF) here.
         // Note: You should fill in the sampled direction *wi and the corresponding *pdf,
         //       and return the sampled BRDF value.
+        
+        // Generate random samples
+        Vector2D sample = sampler.get_sample();
+        double theta = atan(sqrt(-pow(alpha, 2) * log(1-sample.x)));
+        double phi = 2 * PI * sample.y;
+
+        // Sampled microfacet normal
+        Vector3D h = Vector3D(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+        // Reflect wo according to h gives us wi
+        *wi = -wo + 2 * dot(wo, h) * h;
+        wi->normalize();
+
+        // Check wi
+        if (wi->z < 0) {
+          pdf = 0;
+          return Vector3D();
+        }
+
+        // pdf using beckmann NDF
+        double pdf_theta = 2 * sin(theta) * exp(-pow(tan(theta),2)/pow(alpha,2)) / pow(alpha, 2) / pow(cos(theta), 3);
+        double pdf_phi = 1 / (2 * PI);
+
+        double pdf_h = pdf_theta * pdf_phi / sin(theta);
+        *pdf = pdf_h / (4 * dot(*wi, h));
 
         // Default cosine hemisphere sampling code
-        *wi = cosineHemisphereSampler.get_sample(pdf);
+//        *wi = cosineHemisphereSampler.get_sample(pdf);
+  
         return MicrofacetBSDF::f(wo, *wi);
     }
 
